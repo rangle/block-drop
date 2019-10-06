26e9e846287261e218e7ad6224d925f866800cb5
Michael J. Bennett <dev@michaeljbennett.info>
2019-10-06 11:05:25 -0400

    Basic TypeScript web project

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+var el = window.document.createElement('h2');
+el.innerHTML = 'Hello World. Foo.';
+window.document.body.appendChild(el);
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const mode = isProd ? 'production' : 'development';
+const devtool = isProd ? 'hidden-source-map' : 'cheap-module-eval-source-map';
+
+console.log(`building for ${mode}`);
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool,
+  devServer: {
+    contentBase: './dist',
+  },
+  mode,
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/templates/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+};
