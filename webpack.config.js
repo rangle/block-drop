3c24068cf13b17cd767b3f1b245d4e9d2bdc6e3c
Michael J. Bennett <dev@michaeljbennett.info>
2019-10-19 11:33:54 -0400

    Adds a bundle analyzer and test coverage

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const isProd = process.env.NODE_ENV === 'production';
 
 const mode = isProd ? 'production' : 'development';
 const devtool = isProd ? 'hidden-source-map' : 'cheap-module-source-map';
+const allPlugins = [
+  new HtmlWebpackPlugin({
+    template: 'src/templates/index.html',
+  }),
+  new MiniCssExtractPlugin({
+    filename: '[name].css',
+    chunkFilename: '[id].css',
+  }),
+];
+const prodPlugins = [
+  new BundleAnalyzerPlugin({
+    analyzerMode: 'static',
+  }),
+];
+
+const plugins = isProd ? allPlugins.concat(prodPlugins) : allPlugins;
 
 console.log(`building for ${mode}`);
 
@@ -37,15 +54,7 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/templates/index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css',
-    }),
-  ],
+  plugins,
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
